2fac33dc2581b0a44430d462a2b44e6704295dea
Andrey Sipatov <Henrrry52@gmail.com>
2023-06-16 10:54:24 +0400

    Solved hight_tree

---
+++ hight_tree.py
@@ -0,0 +1,21 @@
+import sys
+sys.setrecursionlimit(40000)
+
+N = int(input())
+M = map(int, input().split())
+
+graph = {i: list() for i in range(N)}
+root = None
+for child, parent in enumerate(M):
+    if parent != -1:
+        graph[parent].append(child)
+    else:
+        root = child
+
+def height_tree(vertex):
+    height = 1
+    for children in graph[vertex]:
+        height = max(height, 1 + height_tree(children))
+    return height
+
+print(height_tree(root))
