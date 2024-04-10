8d42aff5b41bf397e0f739479152a85416c64e28
andSpt <henrrry52@gmail.com>
2024-04-10 14:55:33 +0300

    Task 4.1.2 Knapsack solved

---
+++ methods/4.1.2_Knapsack.py
@@ -0,0 +1,26 @@
+import sys
+import heapq
+
+def fractional_knapsack(capacity, values_and_weights):
+    order = [(-v / w, w) for v, w in values_and_weights]
+    heapq.heapify(order)
+
+    acc = 0
+    while order and capacity:
+        v_per_w, w = heapq.heappop(order)
+        can_take = min(w, capacity)
+        acc -= v_per_w * can_take
+        capacity -= can_take
+    return acc
+
+def main():
+    reader = (tuple(map(int, line.split())) for line in sys.stdin)
+    n, capacity = next(reader)
+    values_and_weights = list(reader)
+    assert len(values_and_weights) == n
+    opt_value = fractional_knapsack(capacity, values_and_weights)
+    print(f"{opt_value:.3f}")
+
+
+if __name__ == '__main__':
+    main()
